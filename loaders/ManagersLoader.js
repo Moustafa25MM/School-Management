4552e5496be6be6cda61de306aa6df88c2d8b208
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2024-02-12 01:39:08 +0200

    Mongo Loader

--- loaders/ManagersLoader.js
+++ loaders/ManagersLoader.js
@@ -13,6 +13,7 @@ const TokenManager = require('../managers/token/Token.manager');
 const SharkFin = require('../managers/shark_fin/SharkFin.manager');
 const TimeMachine = require('../managers/time_machine/TimeMachine.manager');
 
+const MongoLoader = require('./MongoLoader');
 const User = require('../managers/entities/user/User.manager');
 
 /**
@@ -36,7 +37,7 @@ module.exports = class ManagersLoader {
       aeon,
       managers: this.managers,
       validators: this.validators,
-      // mongomodels: this.mongomodels,
+      mongomodels: this.mongomodels,
       resourceNodes: this.resourceNodes,
     };
   }
@@ -47,11 +48,11 @@ module.exports = class ManagersLoader {
       customValidators: require('../managers/_common/schema.validators'),
     });
     const resourceMeshLoader = new ResourceMeshLoader({});
-    // const mongoLoader      = new MongoLoader({ schemaExtension: "mongoModel.js" });
+    const mongoLoader = new MongoLoader({ schemaExtension: 'mongoModel.js' });
 
     this.validators = validatorsLoader.load();
     this.resourceNodes = resourceMeshLoader.load();
-    // this.mongomodels          = mongoLoader.load();
+    this.mongomodels = mongoLoader.load();
   }
 
   load() {
