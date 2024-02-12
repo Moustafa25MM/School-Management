b20fdf218d718a9054dc9ee6a242974cf1881276
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2024-02-12 06:06:58 +0200

    school model

---
+++ managers/entities/school/school.mongoModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schoolSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  admin: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: false,
+  },
+});
+
+module.exports = mongoose.model('School', schoolSchema);
